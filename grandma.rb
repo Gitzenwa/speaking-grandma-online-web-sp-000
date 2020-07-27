03f294e3c17002c103a7ae836aed555483fe5565
Ezenwa Onukwue <eonukwue@gmail.com>
2020-07-27 19:50:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.downcase
+  elsif phrase == phrase.downcase
      "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase
     "NO, NOT SINCE 1938!"
