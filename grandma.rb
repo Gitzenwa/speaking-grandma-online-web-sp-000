030ec193191c6081217f908b48ff77bbdff894a4
Ezenwa Onukwue <eonukwue@gmail.com>
2020-07-27 19:44:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@
 def speak_to_grandma(phrase)
   if phrase.downcase
      "HUH?! SPEAK UP, SONNY!"
+  elsif phrase.upcase
+    "NO, NOT SINCE 1938!"
   end
 end
 # Whatever you say to grandma, she should respond with
