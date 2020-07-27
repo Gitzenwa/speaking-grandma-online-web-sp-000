b0f0e40df2c5a144fe29ce9a251ac5f721a4fa78
Ezenwa Onukwue <eonukwue@gmail.com>
2020-07-27 19:49:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.downcase
      "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase
